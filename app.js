449c899a9e4c865dafc43134f4f0e991e4441635
codeboysummer <aw.wahid13@gmail.com>
2022-05-02 19:50:24 -0400

    added img and background functionality

--- app.js
+++ app.js
@@ -1,38 +1,146 @@
-
-
-const numberofitemsel=document.getElementById('num-of-navitems');
-
-const  numberofitems=parseInt(numberofitemsel.value);
-
-const body=document.body;
-const demosection=document.getElementById('demo');
-
-
+const numofnavItems=document.getElementById('num-of-navitems');
+const backgroundcolornav=document.getElementById('colorpicker');
+const textcolornav=document.getElementById('colorpickertext');
 const savebtn=document.getElementById('save');
+const resetbtn=document.getElementById('reset');
+const demosection=document.getElementById('demo');
+const backgroundorimage = document.querySelector('input[name="choice"]:checked');
+
+
+
 savebtn.addEventListener('click',()=>{
-    let ul =document.createElement("ul");
-// append to body 
-ul.classList.add('navbar')
 
+    clear();
+    createNavbar();
+    createbackground();
+    
 
-demosection.insertAdjacentElement('afterbegin',ul);
-
-   //body.append(ul);
-   // create list items 
-   
-   for(let i=0;i<Math.floor(Number(numberofitemsel.value));i++) {
-    let li = document.createElement("li");
-    let navlink=document.createElement('a');
-    navlink.classList.add('navlinks');
-    li.classList.add('navitem');
-
-    navlink.innerText="im a link"
-    navlink.href='#';
-
-    // append to ul
-    ul.appendChild(li);
-    li.appendChild(navlink);
-
-     
-   }
 });
+resetbtn.addEventListener('click',()=>{
+
+    demosection.innerHTML="";
+});
+
+
+let createbackground =()=>{
+// make div and place at the end
+let backgroundDiv= document.createElement("div");
+backgroundDiv.classList.add('background');
+// clear 
+backgroundDiv.innerHTML='';
+
+// placed as last child
+
+demosection.insertAdjacentElement("beforeend",backgroundDiv);
+
+// input element
+let backgroundcolorelement = document.getElementById("backgroundcolorPicker");
+
+
+// make input into background
+backgroundDiv.style.backgroundColor=backgroundcolorelement.value;
+
+
+};
+// create image on choice
+
+let createbackgroundimg=()=>{
+    let backgroundDiv= document.createElement("div");
+    backgroundDiv.classList.add('background');
+    // clear div 
+    backgroundDiv.innerHTML='';
+}
+let createbackgroundimgicon=()=>{
+    
+
+// take input and put as output
+let bodybackgroundel= document.getElementById('background');
+
+
+let backgroundcolorPicker=document.createElement('input');
+bodybackgroundel.innerHTML='';
+backgroundcolorPicker.type='file';
+backgroundcolorPicker.setAttribute("id",'backgroundcolorPicker');
+
+    // append
+    bodybackgroundel.appendChild(backgroundcolorPicker)
+
+
+
+};
+
+
+
+
+// create icon 
+function createbackgroundIcon(){
+    console.log('hi');
+
+
+    // create color input
+    let bodybackgroundel= document.getElementById('background');
+
+
+let backgroundcolorPicker=document.createElement('input');
+bodybackgroundel.innerHTML='';
+backgroundcolorPicker.type='color';
+backgroundcolorPicker.setAttribute("id",'backgroundcolorPicker');
+
+    // append
+    bodybackgroundel.appendChild(backgroundcolorPicker)
+
+
+
+   
+
+
+}
+let createNavbar= ()=>{
+
+    
+    // create nav components
+    const navbar=document.createElement("ul");
+    navbar.classList.add("navbar")
+
+
+
+navbar.style.backgroundColor=backgroundcolornav.value;
+
+
+
+
+    for(var i=0;i<Math.floor(Number(numofnavItems.value));i++){
+
+        let listitem=document.createElement('li');
+        listitem.classList.add("navitem")
+         listitem.innerText=`item`;
+
+
+
+         let navlink=document.createElement('a');
+        navlink.style.color=textcolornav.value
+        navlink.classList.add("navlinks");
+         navlink.innerText=`link`;
+         navlink.href="#";
+
+
+        console.log(textcolornav.value);
+         navbar.appendChild(listitem).appendChild(navlink);
+         demosection.appendChild(navbar);
+         
+
+
+
+    }
+ 
+    }
+
+
+    let clear=()=>demosection.innerHTML='';
+
+
+
+
+
+
+
